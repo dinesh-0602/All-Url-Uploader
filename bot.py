fed25d8e75416b046e6a49dd7cc8c0f455e57df5
DINESH B <81961690+dinesh-0602@users.noreply.github.com>
2024-06-13 14:16:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,19 @@ from pyrogram import Client, idle, __version__
 
 from config import Config
 
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
